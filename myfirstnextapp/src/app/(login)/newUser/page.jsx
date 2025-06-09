ab5ccac06af8599b354b63a50bd073cd48ee6cb2
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-06-09 14:33:39 +0530

    Routing with Pages, Nested Routes & Grouping Pages
    Routing with Pages, Nested Routes & Grouping Pages

---
+++ myfirstnextapp/src/app/(login)/newUser/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>New User</h1>
+    </div>
+  )
+}
+
+export default page
+
+{/* Grouping Pages */}
---
+++ myfirstnextapp/src/app/about/socials/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Socials page</h1>
+    </div>
+  )
+}
+
+export default page
+
+{/* Nested Routes */}

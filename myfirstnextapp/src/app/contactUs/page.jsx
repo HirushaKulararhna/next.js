f5a7ae684200726a1168fda18d0472878d6415f3
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-06-12 08:42:47 +0530

    How to add CSS Styles
    CSS Styles CSS Modules & globals.css

--- myfirstnextapp/src/app/contactUs/page.jsx
+++ myfirstnextapp/src/app/contactUs/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from "./contact.module.css"
 
 const page = () => {
   return (
     <div>
       <h1>Contact us page</h1>
+      <h2><Link href = "/" className={styles.a}>Back to Home</Link></h2>
     </div>
   )
 }
--- myfirstnextapp/src/app/page.jsx
+++ myfirstnextapp/src/app/page.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-export default function Home() {
+ export default function Home() {
   return (
    <>
    <h1>Home page</h1>
 
-   <li><Link href = "/">Home</Link></li>
-   <li><Link href = "/services">Services</Link></li>
-   <li><Link href="/product">Product</Link></li>
-   <li><Link href = "/about">About</Link></li>
-   <li><Link href = "/contactUs">Contact Us</Link></li>
-   <li><Link href = "/about/carrers">Carrers</Link></li>
-   <li><Link href = "https://chat.deepseek.com/" target="_blank">DeepSeek</Link></li>
+   <li><Link href = "/" className={styles.Link} >Home</Link></li>
+   <li><Link href = "/services" className={styles.Link} >Services</Link></li>
+   <li><Link href="/product" className={styles.Link} >Product</Link></li>
+   <li><Link href = "/about" className={styles.Link} >About</Link></li>
+   <li><Link href = "/contactUs" className={styles.Link} >Contact Us</Link></li>
+   <li><Link href = "/about/carrers" className={styles.Link} >Carrers</Link></li>
+   <li><Link href = "https://chat.deepseek.com/" target="_blank" className={styles.specialLink} >DeepSeek</Link></li>
 
    </>
     
